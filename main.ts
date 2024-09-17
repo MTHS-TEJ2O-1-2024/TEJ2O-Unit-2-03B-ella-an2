580037feec0bc35e51fa03f9bd65038b1b613690
ella-an2 <ella.an@mths.ca>
2024-09-17 11:14:33 -0400

    added the units

--- main.ts
+++ main.ts
@@ -9,10 +9,10 @@ basic.clearScreen()
 basic.pause(1000)
 
 
-basic.showString('Area equals: 5 x 3 =' + (5 * 3).toString())
+basic.showString('Area equals: 5 x 3 =' + (5 * 3).toString() + 'cm ^ 2')
 basic.pause(100)
 basic.clearScreen()
 
-basic.showString('Perimeter equals: 5 + 5 + 3 + 3 =' + (5 + 5 + 3 + 3).toString())
+basic.showString('Perimeter equals: 5 + 5 + 3 + 3 =' + (5 + 5 + 3 + 3).toString() + 'cm')
 basic.pause(100)
 basic.clearScreen()
