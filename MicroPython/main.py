7561502336132da4eeddecfbf3907d779ca03ec6
ella-an2 <ella.an@mths.ca>
2024-09-17 15:23:21 +0000

    Programmed to calculate the perimeter and area

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -5,6 +5,16 @@ This module is a Micro:bit MicroPython program
 """
 
 from microbit import *
+from time import sleep
 
 
-display.scroll("Hello, World!")
+display.clear()
+sleep(1)
+
+display.scroll("Area equals: 5 x 3 =" + str(5 * 3) + "cm ^ 2")
+sleep(0.5)
+display.clear
+
+display.scroll("Perimeter equals: 5 + 5 + 3 + 3 =" + str(5 + 5 + 3 + 3) + "cm")
+sleep(0.5)
+display.clear
