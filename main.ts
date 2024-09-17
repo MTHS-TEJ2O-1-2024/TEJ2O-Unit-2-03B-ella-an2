fa575fcc0fa553a06a100faf065fd31a54b296ee
ella-an2 <ella.an@mths.ca>
2024-09-17 11:08:03 -0400

    Programmed the computer to calculate perimeter and area

--- main.ts
+++ main.ts
@@ -1,8 +1,18 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Ella
+ * Created on: Sep 2024
+ * This program calculate the area and perimeter
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.pause(1000)
+
+
+basic.showString('Area equals: 5 x 3 =' + (5 * 3).toString())
+basic.pause(100)
+basic.clearScreen()
+
+basic.showString('Perimeter equals: 5 + 5 + 3 + 3 =' + (5 + 5 + 3 + 3).toString())
+basic.pause(100)
+basic.clearScreen()
